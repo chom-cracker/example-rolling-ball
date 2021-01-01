772d7a58e7bb2f864eb61139c5faee46da80b560
GHeeJeon <jhjeon_99@naver.com>
2021-01-02 02:43:49 +0900

    upload
    in progress

---
+++ example_rolling_ball/Assets/Scripts/EnterTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Capsule"))
+        {
+            print("Hi Capsule!");
+        }
+    }
+
+
+
+
+}
---
+++ example_rolling_ball/Assets/Scripts/RolllingBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RolllingBall : MonoBehaviour
+{
+    public float speed = 3f;
+
+    public Rigidbody rigid;
+    public float horizontal;
+    public float vertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0.0f, vertical);
+
+        rigid.AddForce(direction * speed);
+    }
+}
